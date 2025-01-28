bbfa9e537611b24144f01d0810dd8de4a155e79a
Ashley Conner <108750120+valardohaeris2@users.noreply.github.com>
2025-01-28 17:44:02 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import time
 from . import models
 from .database import engine, SessionLocal 
 
-models.Base.metadata.create_all(bind=engine)
+models.Base.metadata.create_all(bind=engine) 
 
 app = FastAPI()
 
