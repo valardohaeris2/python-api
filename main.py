d25b924b4a3cf81117c11baa88a501949ba8f869
Ashley Conner <108750120+valardohaeris2@users.noreply.github.com>
2025-03-11 12:30:43 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from fastapi.params import Body
 from pydantic import BaseModel
 from random import randrange
 import psycopg2
-from psycopg2.extras import RealDictCursor   
+from psycopg2.extras import RealDictCursor    
 import time
 from . import models
 from .database import engine, SessionLocal  
